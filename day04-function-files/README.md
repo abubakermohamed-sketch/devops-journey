82ca91ab542db2c78c46ae67cb66fac392e869fe
Mohamed Alhadi <mohammed_faddel@yahoo.com>
2026-07-22 06:54:59 -0400

    Day 4: Functions and file I/O

---
+++ day04-function-files/README.md
@@ -0,0 +1,2 @@
+# Day 4:Funtions and File I/O
+Practiced defining functions, return values, reading files with open()/ with, splitines()
---
+++ day04-function-files/classfy_servers.py
@@ -0,0 +1,24 @@
+def classify_server(server_name):
+    """Takes a server name and returns its type as a string."""
+    if "web" in server_name:
+        return "Web Server"
+    elif "db" in server_name:
+        return "Database Server"
+    elif "cache" in server_name:
+        return "Cache Server"
+    else:
+        return "Unknown Type"
+
+
+def main():
+    with open("servers.txt", "r") as f:
+        servers = f.read().splitlines()  # splitlines removes the \n from each line
+
+    for server in servers:
+        server_type = classify_server(server)
+        print(f"{server} -> {server_type}")
+
+    print(f"\nTotal servers processed: {len(servers)}")
+
+
+main()
